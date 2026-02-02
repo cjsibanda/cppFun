71ea8fd0b84e7b5c90c741f386d683f22f2e0b4a
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-02-02 02:52:47 +0000

    NameTag DIY

---
+++ shiboboC/NameTag.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TagList.h"
+#include "NameTag.h"
+using namespace std;
+namespace shibobo {
+    //Private Methods
+
+    //Public methods
+
+    //constructor
+
+    //Destructor
+}
---
+++ shiboboC/NameTag.h
@@ -0,0 +1,20 @@
+#ifndef SHIBOBO_CC_H
+#define SHIBOBO_CC_H
+#include <iostream>
+
+namespace shibobo {
+    class Nametag {
+        //Private attributes
+
+        //private methods
+
+        //public methods
+
+        //constructors
+
+        //Destructor
+
+
+    };
+}
+#endif //!SHIBOBO_CC_H
