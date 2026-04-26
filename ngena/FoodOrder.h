f66fc07e3e4d6125e1a9a44673aa263e0923ae91
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-04-26 00:12:39 +0000

    updated FoodOrder.h

--- ngena/FoodOrder.h
+++ ngena/FoodOrder.h
@@ -1,24 +1,27 @@
-//Something like this...
-//g++ -Wall -std=c++17 -g -o ws file1.cpp file2.cpp
-#include <iostream>
+//try --> g++ -Wall -std=c++17 -g -o ws file1.cpp file2.cpp
 #define SIBANDA_FOODORDER_H
 #ifndef SIBANDA_FOODORDER_H //header guard
 
-// extern double g_taxrate;
-// extern g_dailydiscount; 
+#include <iostream>
+
+extern double g_taxrate;
+extern g_dailydiscount; 
 
 namespace sibanda {
 
 class FoodOrder {
-    string m_name[10];
-    string m_length;  //c_ctyle null-terminated string of any length?
-    double m_price;
-    bool isSpecial;       
+    char m_name[10];
+    char* m_length{};  
+    double m_price{};
+    bool isSpecial{};       
   public:
     FoodOrder(); //Default constructor
-    void read(std::istream&);
-    void display(std::ostream&, FoodOrder&) const //const?
-}
+    FoodOrder(const FoodOrder& src);
+    FoodOrder& operator=(const FoodOrder& src);
+    ~FoodOrder();
+    void read(std::istream &is);
+    void display() const 
+  };
 
 }
 #endif
