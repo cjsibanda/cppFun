aecd3a11a72a04e992a186932191e31605d337e5
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-04-27 00:50:10 +0000

    created TimedTask & ProteinDatabase files

---
+++ ngena2/TimeTask.cpp
@@ -0,0 +1,8 @@
+/*
+-Retrieving records from a file using input file stream object
+-Move and Copy Semantics: Protein Sequence
+-Copy Semantics for a class with resource
+-Move semantics for a class with a resource
+-std::chrono
+--> Question: Why is there big difference between copy and move ops
+*/
