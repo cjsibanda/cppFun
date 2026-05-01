d9868a43d9646779f46e0a30c5ae50153f6398db
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-01 16:20:29 +0000

    started Set.h

---
+++ ngena3/Set.h
@@ -0,0 +1,17 @@
+#ifndef SIBANDA_SET_H
+#define SIBANDA_SET_H
+include <cmath>
+#include "Collection.h" //added
+
+namespace sibanda {
+    //to be continued
+    // Set inherits from Collection
+    template<typename T>
+    class Set : public Collection<T, SET_CAPACITY>
+    {
+        //....................
+        //....................
+    }
+  
+}
+#endif
