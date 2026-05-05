15486c56ae60313e020b69182368d49fcd393a20
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-05 02:09:48 +0000

    started Pair.h

---
+++ ngena3/Pair.h
@@ -0,0 +1,25 @@
+//templates
+#ifndef SIBANDA_PAIR_H
+#define SIBANDA_PAIR_H
+#include <string>
+#include <iomanip>
+
+namespace sibanda
+{
+    /*
+    -> for representing key-value pairs
+    -> two objects of type Pair are considered equal if they have the same key 
+    */
+    
+    class Pair
+    {
+        std::string m_key{};
+        std::string m_value{};
+    public:
+        /*
+        To be continued...
+        // prototypes
+        */
+    };
+}
+#endif
