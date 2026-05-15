ca1e5d82fe6db89409482a527f91e7e1dec5a4f3
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-15 00:47:01 +0000

    updated proteinDatabase.h (declarations)

---
+++ ngena_test3/proteinDatabase.h
@@ -0,0 +1,51 @@
+//Copy & Move semantics
+// ProteinDatabase.h
+
+#ifndef SENECA_PROTEINDATABASE_H
+#define SENECA_PROTEINDATABASE_H
+
+#include <string>
+
+namespace seneca 
+{
+    /*****************************************
+    * -> ProteinDatabase manages two paralel dynamically allocated arrays
+    * ... of type std::string 
+    * -> one array will store the Unique IDs of the proteins
+    * -> second array will store sequence of amino acids making protein
+    * -> class keeps track of the number of proteins currently stored
+    ******************************************/
+    class ProteinDatabase
+    {
+        std::string* m_UniqueIDs{ nullptr };
+        std::string* m_aminoSequences{ nullptr };
+        size_t m_cnt{ 0 };
+    public:
+       /////////////////////////////////////////
+       // -> no arg, 1-arg constructor
+       // -> Rule of 5
+       /////////////////////////////////////////
+        ProteinDatabase();
+        ProteinDatabase(const std::string&);
+        ProteinDatabase(const ProteinDatabase&);
+        ProteinDatabase(ProteinDatabase&&) noexcept;
+        ProteinDatabase& operator=(const ProteinDatabase&);
+        ProteinDatabase& operator=(ProteinDatabase&&) noexcept;
+
+        ~ProteinDatabase();
+
+        /***************************************************
+        * -> size_t size(): query returns number of sequences
+        * in the current object
+        * -> std::string operator[](size_t): query that returns
+        * copy of the protein sequence at index received as parameter
+        * -> std::string getUID(size_t): a query that returns copy of
+        * protein ID at index received as function parameter.  
+        *****************************************************/
+        size_t size() const { return m_count }
+        std::string operator[](size_t idx) const;
+        std::string getUID(size_t idx) const;
+
+    };
+}
+#endif
