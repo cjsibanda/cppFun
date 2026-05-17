00425e09b4804cdfa03c748c5d595020f0672f60
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-17 20:03:13 +0000

    started event.h

---
+++ ngena_test4/event.h
@@ -0,0 +1,13 @@
+#ifndef SENECA_EVENT_H
+#define SENECA_EVENT_H
+
+#include <iostream>
+
+namespace seneca {
+    class Event {
+        /////////////////////////////////////////
+        //Stores info about a single event that
+        // happended during execution of program
+        ////////////////////////////////////////
+    }
+}
