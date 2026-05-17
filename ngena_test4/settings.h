45db19fc114cdf7b947058176626420f623aabe2
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-17 19:59:39 +0000

    started settings.h

---
+++ ngena_test4/settings.h
@@ -0,0 +1,22 @@
+#ifndef SENECA_SETTINGS_H
+#define SENECA_SETTINGS_H
+
+#include <iostream>
+
+namespace seneca {
+    /******************************** 
+    * -> contains functionality regarding configuration
+    * of the application
+    ********************************/
+    // global varibale g_settings
+    class Settings {
+        bool m_show;
+        bool m_verbose;
+        bool m_time_units;
+    }
+    struct Setting {
+
+    }
+
+}
+#endif
