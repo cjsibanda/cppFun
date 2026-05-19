39cd4134043c2f2097559d7523aab9568907b028
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-19 21:42:56 +0000

    started Event.cpp

---
+++ ngena_test4/Event.cpp
@@ -0,0 +1,10 @@
+#include <fstream> //??
+
+namespace seneca {
+    
+    /******************************
+    * If you want to go fast, go alone.
+    * If you want to go far, go together.
+    * I am because we are
+    *******************************/
+}
