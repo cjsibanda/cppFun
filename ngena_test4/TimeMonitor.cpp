0824bf00f19d6dda857b03e77bbb32296da4876d
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-19 21:47:57 +0000

    added TimeMonitor.cpp

---
+++ ngena_test4/TimeMonitor.cpp
@@ -0,0 +1,9 @@
+#inlcude <fstream> //check
+
+namespace seneca {
+    /******************************
+    * If you want to go fast, go alone.
+    * If you want to go far, go together.
+    * I am because we are
+    *******************************/
+}
