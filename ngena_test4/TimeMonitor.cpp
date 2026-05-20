d0fc76e6c8e7fcd0757d6d5a8820767909a09757
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-20 08:13:23 +0000

    implemented TimeMonitor.cpp

--- ngena_test4/TimeMonitor.cpp
+++ ngena_test4/TimeMonitor.cpp
@@ -1,9 +1,21 @@
-#inlcude <fstream> //check
+#include "TimeMonitor.h"
 
 namespace seneca {
-    /******************************
-    * If you want to go fast, go alone.
-    * If you want to go far, go together.
-    * I am because we are
-    *******************************/
+    void TimeMonitor::startEvent(const char* name) {
+        if (name) {
+            m_event_name = name;
+        }
+        //Captureing time-stamp
+        m_start_time = std::chrono::steady_clock::now();
+    }
+
+    Event TimeMonitor::stopEvent() {
+        auto end_time = std::chrono::steady_clock::now();
+
+        //Calculate raw difference in nanoseconds
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - m_start_time);
+
+        //construct and return the completed event record
+        return Event(m_event_name.c_str(), duration);
+    }
 }
