4751ccb23959726f02ba721d9c45e36e971ac5be
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-05-20 05:57:57 +0000

    updated Event.h no rule of 5 needed

--- ngena_test4/Event.h
+++ ngena_test4/Event.h
@@ -2,26 +2,25 @@
 #define SENECA_EVENT_H
 
 #include <iostream>
+#include <string>
+#include <chrono>
 
 namespace seneca {
-    class Event {
-        std::string* m_eventName{ nullptr };
-        /////////////////////////////////////////
+    /////////////////////////////////////////
         //Stores info about a single event that
         // happended during execution of program
         ////////////////////////////////////////
+    class Event {
+        std::string m_eventName{};
+        std::chrono::nanoseconds m_duration{ 0 };
     public:
-        Event();
+        Event() = default;
         //////////////////////////////////////////////////
         // Instantiates the current instance with
         // the values of the parameters
         //////////////////////////////////////////////////
+        //no need fo rule of 3/5? no dma?
         Event(const char* name, const std::nanosecond& duration);
-        Event(const Event&);
-        Event(Event&&) noexcept;
-        Event& operator=(const Event&);
-        Event& operator=(Event&&) noexcept;
-        ~Event()
         /*******************************************
         * -> Friend Helper operator<<
         * ->inserts into the first parameter an object
@@ -29,7 +28,8 @@ namespace seneca {
         * the format:
         * Counter: EVENT_NAME -> DURATION TIME_UNITS
         *********************************************/
-        friend std::ostream& operator<<(std::ostream out, const Event& event);
+        friend std::ostream& operator<<(std::ostream& os, const Event& event);
 
     }
 }
+#endif
