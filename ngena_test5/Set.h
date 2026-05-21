f8b9cfc0dc0ba65cc4091da091ee5aee2abba8d9
cjsibanda <anesib100@gmail.com>
2026-05-21 05:48:50 -0400

    started Set.h

---
+++ ngena_test5/Set.h
@@ -0,0 +1,13 @@
+#ifndef SENECA_SET_H
+#define SENECA_SET_H
+
+include <iostream>
+
+namespace seneca {
+    /************************************
+    * If you want to go fast, go alone.
+    * If you want to go far, go together.
+    * I am because we are
+    **************************************/
+}
+#endif
