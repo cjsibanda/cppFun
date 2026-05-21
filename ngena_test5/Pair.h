7cf170631fb7e01c6861fdbce65ae4ba5774bb28
cjsibanda <anesib100@gmail.com>
2026-05-21 05:52:31 -0400

    added Pair.h

---
+++ ngena_test5/Pair.h
@@ -0,0 +1,14 @@
+#ifndef SENECA_PAIR_H
+#define SENECA_PAIR_H
+
+#include <iostream>
+#include <string>
+
+namespace seneca {
+  /************************************
+    * If you want to go fast, go alone.
+    * If you want to go far, go together.
+    * I am because we are
+    **************************************/
+}
+#endif
