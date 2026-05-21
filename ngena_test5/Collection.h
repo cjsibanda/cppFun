80a09c45e98cfb07caabc868e405128addd9603b
cjsibanda <anesib100@gmail.com>
2026-05-21 05:50:46 -0400

    added Collection.h

--- ngena_test5/Collection.h
+++ ngena_test5/Collection.h
@@ -1,13 +1,14 @@
-#ifndef SENECA_SET_H
-#define SENECA_SET_H
+#ifndef SENECA_COLLECTION_H
+#define SENECA_COLLECTION_H
 
-#include <iostream>
+include <string>
 
-namespace seneca {
+namespace {
     /************************************
     * If you want to go fast, go alone.
     * If you want to go far, go together.
     * I am because we are
     **************************************/
+
 }
 #endif
