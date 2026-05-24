b2cf030fe770ad34b9ab39408655c8e484d08ea3
cjsibanda <anesib100@gmail.com>
2026-05-24 06:28:03 -0400

    updated collection.h

--- ngena_test5/Collection.h
+++ ngena_test5/Collection.h
@@ -1,14 +1,28 @@
 #ifndef SENECA_COLLECTION_H
 #define SENECA_COLLECTION_H
 
-include <string>
+#include <string>
 
 namespace {
-    /************************************
-    * If you want to go fast, go alone.
-    * If you want to go far, go together.
-    * I am because we are
-    **************************************/
-
-}
+    
+    /**************************************************
+    * -> Colelctions: represents a family of collections
+    * ... of elements of any data type
+    * Template nemed Collection
+    * [T]: the type of any element in the collection
+    * [CAPACITY]: the capacity of the collection
+    * ... this is the max num of elements that can be added
+    * -> keep track of current number of elements stored
+    ****************************************************/
+    class Collection {
+        //private memebers
+        // an object of type T stat store default value
+    public: 
+        void size();
+        void display();
+        bool add(const T& item);
+        //operator[];
+        virtual ~Collection(); 
+    }
+};
 #endif
