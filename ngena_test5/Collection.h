01c76da134acfbb2fbd74240b0f983e0bc65d0d3
cjsibanda <anesib100@gmail.com>
2026-05-25 05:08:42 -0400

    updated - do I need .cpp for template class?

--- ngena_test5/Collection.h
+++ ngena_test5/Collection.h
@@ -14,15 +14,31 @@ namespace {
     * ... this is the max num of elements that can be added
     * -> keep track of current number of elements stored
     ****************************************************/
+    template <typename T, int CAPACITY>
     class Collection {
         //private memebers
         // an object of type T stat store default value
+        // allows duplicates?
+        T m_items[CAPACITY]{};
+        size_t m_count{}; //keep track of current num of elements stored
+
+        static T m_defaultValue;
+
     public: 
-        void size();
-        void display();
-        bool add(const T& item);
+        void size();  //fix
+        void display(); //fix
+        bool add(const T& item); //fix
         //operator[];
-        virtual ~Collection(); 
-    }
-};
+        virtual ~Collection() = default; //okay? 
+    };
+
+    // Template Assumptions here
+    template<typename T, int CAPACITY>
+    //....
+
+    template<>
+    //....
+}
 #endif
+
+//is there need for .cpp file?? .. why not?
