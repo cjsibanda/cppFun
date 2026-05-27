0dd54fff9b032fddcc5fc556d7ab96e2522d394e
cjsibanda <anesib100@gmail.com>
2026-05-26 21:32:17 -0400

    started confirmationOrder.h

---
+++ ngena_test6/confirmationOrder.h
@@ -0,0 +1,18 @@
+#ifndef SENECA_CONFIRMATIONORDER_H
+#define SENECA_CONFIRMATIONORDER_H
+
+#include <iostream>
+#include <string>
+
+namespace seneca {
+    class confirmationOrder
+    {
+        /***************************
+        * Better a dry crust with peace
+        * and quiet than a house full of 
+        * feasting, with strife
+        ***************************/
+    };
+
+}
+#endif
