63330aba5c53e0423c475d6dbb56b890d0b55c45
cjsibanda <anesib100@gmail.com>
2026-05-28 23:11:03 -0400

    updated toy.h

--- ngena_test6/toy.h
+++ ngena_test6/toy.h
@@ -5,17 +5,31 @@
 #include <string> //???
 
 namespace seneca {
+    /*******************************************************
+    * ->Toy Module includes the definition of a class that manages
+    * info about single toy
+    * id, name, number of items, price, harmonized sales tax
+    ********************************************************/
     class Toy {
-        /*************************************
-        * If you want to go fast, go alone.
-        * If you want to go far, go together
-        * I am because we are
-        * Ubuntu
-        *************************************/
+        unsigned int m_id{};
+        std::string m_name{};
+        unsigned int m_numItems{};
+        double m_price{};
+
+        static constexpr double HST = 0.13;
+        
     public:
        Toy() = default; //default constructor
+
+       //constructor that parses a string
        Toy(const std::string& toy);
+
+       //modifier
        void update(int numItems);
+
+
+       //output helper
+       friend std::ostream& operator<<(std::ostream& os, const Toy& toy);
        
     }
 
