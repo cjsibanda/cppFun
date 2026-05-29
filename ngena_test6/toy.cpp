0321ac5cf5882efdd777e0ed2a745a1cc2a3f377
cjsibanda <anesib100@gmail.com>
2026-05-28 23:43:08 -0400

    started toy.cpp

---
+++ ngena_test6/toy.cpp
@@ -0,0 +1,10 @@
+#include <iomanip> //??
+#include "toy.h"
+
+namespace seneca {
+    /**************************************
+    * If your only tool is a hammer,
+    * you will see every problem as a nail
+    ***************************************/
+    
+}
