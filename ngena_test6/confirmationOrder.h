9eea18e22fd9d9c8ba11353a562d20e3fe255f70
cjsibanda <anesib100@gmail.com>
2026-05-28 23:38:02 -0400

    updated ConfirmationOrder.h (Aggregation)

--- ngena_test6/confirmationOrder.h
+++ ngena_test6/confirmationOrder.h
@@ -2,16 +2,55 @@
 #define SENECA_CONFIRMATIONORDER_H
 
 #include <iostream>
-#include <string>
+#include "toy.h"
+
+
 
 namespace seneca {
+    /***************************************************************
+    * ConfrimationOrder -> USES AGGREGATION
+    * Only stores addresses
+    * Purpose of the class is to contact the parents of the
+    * children who want to shop and retreive approval before placing
+    * an order
+    *****************************************************************/
     class confirmationOrder
     {
-        /***************************
-        * Better a dry crust with peace
-        * and quiet than a house full of 
-        * feasting, with strife
-        ***************************/
+        const Toy** m_toys{};
+        size_t m_numToys{};
+    public:
+        //default constructor
+        ConfirmationOrder() = default;
+
+        //copy constructor
+        ConfirmationOrder(const ConfrimationOrder& src);
+
+        //copy assignment operator
+        ConfirmationOrder& operator=(const ConfirmationOrder& src);
+
+        // move constructor
+        ConfirmationOrder(ConfirmationOrder&& src) noexcept;
+
+
+        //move assignment operator
+        ConfirmationOrder& operator=(ConfirmationOrder&& src) noexcept;
+
+        //desrtuctor
+        ~ConfirmationOrder();
+
+
+        //add toy address
+        ConfirmationORder& operator+=(const Tpy& toy);
+
+        //remove toy address
+        ConfirmationOrder& operator-=(const Toy& toy);
+
+        //output helper
+        friend std::ostream& operator<<(
+            std::ostream& os,
+            const ConfirmationOrder& order
+            );
+
     };
 
 }
