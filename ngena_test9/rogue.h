b0139a5529211c9c76032cc9073c64d131944162
cjsibanda <anesib100@gmail.com>
2026-06-07 06:44:05 -0400

    updated rogue and team

--- ngena_test9/rogue.h
+++ ngena_test9/rogue.h
@@ -1,4 +1,62 @@
-/***************************************
-* If you want to go fast, go alone
-* If you want to go far, go together
-****************************************/
+#ifndef SENECA_ROGUE_H
+#define SENECA_ROGUE_H
+#include "characterTpl.h"
+#include "weapons.h"
+
+namespace seneca {
+    /*****************************************************
+    * Rogue is derived from CharacterTpL<T>
+    * concrete class implementing babarian-specific logic
+    * For Template paramerters:
+    * [T]: the type of object storing the healty
+    * FirstAbility_t: the type implementing the first special ability
+    * SecondAbility_t: the type implmenting second ability
+    *****************************************************/
+    template <typename T, typename FirstAbility_t, typename SecondAbility_t>
+    class Rogue : public CharacterTpl<T> {
+        int m_baseDefense; //represents basic defense
+        int m_baseAttack; //represents basick attack power
+        FirstAbility_t m_firstAbility;
+        SecondAbility_t m_secondAbility;
+        seneca::Dagger m_weapon; //?? (just say Dagger?)
+    public:
+        //initializes to values in parameters
+        Rogue(const char* name, int healthMax, int baseAttack, int baseDefense)
+            : CharacterTpl<T>(name, healthMax), m_baseAttack(baseAttack), m_baseDefense(baseDefense) {}
+
+
+            
+        int getAttackAmnt() const override { return m_baseAttack + 2 * static_cast<int>(static_cast<double>(m_weapon)); }
+        int getDefenseAmnt() const override { return m_baseDefense; }
+
+        // dynamically creates a copy of the current instance
+        // returns its address to the client
+        Character* clone() const override { return new Rogue<T, FirstAbility_t, SecondAbility_t>(*this); }
+
+        //Attacks the enemy received in parameter
+        // inflicts damage to enemy
+        //uses first special ability fo activate and benefical effects on self
+        void attack(Character* enemy) override {
+            std::cout << getName() << " is attacking " << enemy->getName() << "." << std::endl;
+            m_firstAbility.useAbility(this);
+            m_secondAbility.useAbility(this);
+            int dmg = getAttackAmnt();
+            m_firstAbility.transformDamageDealt(dmg);
+            m_secondAbility.transformDamageDealt(dmg);
+            std::cout << "    Rogue deals " << dmg << " melee damage!" << std::endl;
+            enemy->takeDamage(dmg);
+        }
+
+        //override
+        //
+        void takeDamage(int dmg) override {
+            std::cout << getName() << " is attacked for " << dmg << " damage." << std::endl;
+            std::cout << "    Rogue has a defense of " << getDefenseAmnt() << ". Reducing damage received." << std::endl;
+            int actual = (dmg - getDefenseAmnt() < 0) ? 0 : dmg - getDefenseAmnt();
+            m_firstAbility.transformDamageReceived(actual);
+            m_secondAbility.transformDamageReceived(actual);
+            CharacterTpl<T>::takeDamage(actual);
+        }
+    };
+}
+#endif
--- ngena_test9/team.h
+++ ngena_test9/team.h
@@ -1,1 +1,52 @@
-//Smooth seas do not make skillful sailors
+#include <string>
+#include "character.h"
+
+namespace seneca {
+    /************************************************************* 
+    * Team manages a dynamically allocated collection of characters
+    * class should work wity an array of pointers to Character
+    * -> stores the address of the array and a string with name of team
+    * Team is in COMPOSITION relationship with character
+    **************************************************************/
+    class Team {
+        Character** m_members = nullptr;
+        size_t m_size = 0;
+        std::string m_name;
+    public:
+        Team(const char* name) : m_name(name) {}
+        // Rule of 5 Implementation
+        ~Team() { for(size_t i=0; i<m_size; ++i) delete m_members[i]; delete[] m_members; }
+        // Implement Copy/Move constructors and assignment operators here
+        //rule of 5
+        
+        // adds the character received as parameter
+        //ONLY if team doesn't have character with the same name
+        //resize the array if necessary
+        void addMember(const Character* c) {
+            for(size_t i=0; i<m_size; ++i) if(m_members[i]->getName() == c->getName()) return;
+            Character** temp = new Character*[m_size + 1];
+            for(size_t i=0; i<m_size; ++i) temp[i] = m_members[i];
+            temp[m_size++] = c->clone();
+            delete[] m_members;
+            m_members = temp;
+        }
+
+        //need remove member
+ 
+        /***************************************************** 
+        *
+        *prints to the screen the content of the current object
+        *uses the format...
+        * [Team] TEAM_NAME<endl>
+        *   1: FIRST_CHARACTER<endl>
+        *   2: SECOND_CHARACTER<endl>
+        *   3: THIRD_CHARACTER<endl>
+        *    ...
+        ******************************************************/
+        void showMembers() const {
+            if(m_size == 0) { std::cout << "No team." << std::endl; return; }
+            std::cout << "[Team] " << m_name << std::endl;
+            for(size_t i=0; i<m_size; ++i) std::cout << "    " << i+1 << ": " << *m_members[i] << std::endl;
+        }
+    };
+}
