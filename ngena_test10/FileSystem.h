62face965cd8ff2eb8ff9c00a963d1388479396d
cjsibanda <anesib100@gmail.com>
2026-06-10 07:20:41 -0400

    started FileSystem.h

---
+++ ngena_test10/FileSystem.h
@@ -0,0 +1,14 @@
+#ifndef SENECA_FILESYSTEM_H
+#define SENECA_FILESYSTEM_H
+
+#include <vector>
+#inlcude <ostream>
+
+namespace seneca {
+    class Filesystem {
+        /***********************************
+        * You always learn a lot more when 
+        *  you lose than when you win 
+        *************************************/
+    }
+}
