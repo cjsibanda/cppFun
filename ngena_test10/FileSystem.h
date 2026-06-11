e896e550a36869fcb5c9070a831e3274f5dd184d
cjsibanda <anesib100@gmail.com>
2026-06-11 05:37:19 -0400

    updated Filesytem.h

--- ngena_test10/FileSystem.h
+++ ngena_test10/FileSystem.h
@@ -4,11 +4,34 @@
 #include <vector>
 #inlcude <ostream>
 
+#include "Resource.h"
+#include "Directory.h"
+#include "File.h"
+#include "Flags.h"
+
 namespace seneca {
+    /***************************************************************
+    * Filesystem defines a class that represents a simple UNIX-like 
+    * filesystem. Lets you move through root directory which holds all
+    * filesystems resources
+    ****************************************************************/
     class Filesystem {
-        /***********************************
-        * You always learn a lot more when 
-        *  you lose than when you win 
-        *************************************/
-    }
+        Directory* m_root = nullptr; //a pointer to the fielsystem root directory 
+        Directory* m_current = nullptr; //pointer to current working directory
+
+       // std::string_trim(std::string); ???
+    public:
+       Filesystem(const char*, const std::string & = " "); //custom constructor
+       Filesystem(const Filesystem&) = delete; //no copy ops
+       Filesystem& operator=(const Filesystem&) = delete; //no copy ops
+       Filesystem(Filesystem&&) noexcept; //move constructor
+       Filesystem& operator=(Filesystem&&) noexcept; //move assignment ops
+       ~Filesystem(); //destructor
+
+       Filesystem& operator+=(Resource*); //adds resource to current directory
+       Directory* change_directory(const std::string & = ""); //change current directory
+       Directory* get_current_directory() const;
+    };
 }
+
+#endif
