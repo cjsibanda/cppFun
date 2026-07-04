37b05f2c8a308d40ee55297ff37a56b3468d31b7
cjsibanda <anesib100@gmail.com>
2026-07-04 05:38:43 -0400

    started settings.h

---
+++ ngena_test15/settings.h
@@ -0,0 +1,20 @@
+#ifndef SENECA_SETTINGS_H
+#define SENECA_SETTINGS_H
+
+#include <iostream>
+#include <vector> //??
+#include <string> //??
+
+//define global variable g_settings 
+
+/*********************************************************
+* Contains the functionality regaerding the configuration
+* of the application. 
+*********************************************************/
+struct Settings 
+{
+    int m_maxSummaryWidth{80};
+    bool m_tableView{false};
+    //YNWA
+}
+#endif
