37c522d24c8adf20eaeeb3b0e3e5fdd800153d1b
cjsibanda <anesib100@gmail.com>
2026-07-05 13:03:29 -0400

    started tvShow module

---
+++ ngena_test15/tvShow.h
@@ -0,0 +1,19 @@
+#ifndef SENECA_TVSHOW_H
+#define SENECA_TVSHOW_H
+
+#include <vector>
+#include <string>
+#include <list>
+#include <ostream>
+
+namespace seneca 
+{
+    class TvShow
+    {
+        /*************************************************
+        *  It is a child who has never travelled
+        * who says only his mother prepares tasty meals :)
+        **************************************************/
+    };
+}
+#endif
