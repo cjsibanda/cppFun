a482fa0b95792e379341093ebf6d530187b528a9
cjsibanda <anesib100@gmail.com>
2026-07-06 05:57:05 -0400

    continued tvShow module

---
+++ ngena_test15/tvShow.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <string>
+
+#include "tvShow.h"
+
+namespace seneca
+{
+    /*****************************************************
+    * TvShow that stores information for a tv show
+    * m_id, title, year of release, the summary, m_episods
+    ******************************************************/
+    //???
+
+    /*****************************************************
+    * override this function to print the information
+    * about a single book 
+    *****************************************************/
+    void TvShow::display(std::ostream& out) const
+    {
+      if (g_settings.m_tableView)
+      {
+        out << "S | ";
+        out << std::left << std::setfill('.');
+        out << std::setw(50) << this->getTitle() << " | ";
+        out << std::right << std::setfill(' ');
+        out << std::setw(2) << this->m_episodes.size() << " | ";
+        out << std::setw(4) << this->getYear() << " | ";
+        out << std::left;
+        if (g_settings.m_maxSummaryWidth > -1)
+        {
+          if (static_cast<short>(this->getSummary().size()) <= g_settings.m_maxSummaryWidth)
+            out << this->getSummary();
+         else
+            out << this->getSummary().substr(0, g_settings.m_maxSummaryWidth - 3) << "...";
+        }
+        else
+          out << this->getSummary();
+        out << std::endl;
+      }
+      else
+      {
+        size_t pos = 0;
+        out << this->getTitle() << " [" << this->getYear() << "]\n";
+        out << std::setw(this->getTitle().size() + 7) << std::setfill('-') << "" << '\n';
+        while (pos < this->getSummary().size())
+        {
+          out << "    " << this->getSummary().substr(pos, g_settings.m_maxSummaryWidth) << '\n';
+          pos += g_settings.m_maxSummaryWidth;
+        }
+        for (auto& item : m_episodes)
+        {
+          out << std::setfill('0') << std::right;
+          out << "    " << 'S' << std::setw(2) << item.m_season
+            << 'E' << std::setw(2) << item.m_numberInSeason << ' ';
+          if (item.m_title != "")
+            out << item.m_title << '\n';
+          else
+            out << "Episode " << item.m_numberOverall << '\n';
+
+          pos = 0;
+          while (pos < item.m_summary.size())
+          {
+            out << "            " << item.m_summary.substr(pos, g_settings.m_maxSummaryWidth - 8) << '\n';
+            pos += g_settings.m_maxSummaryWidth - 8;
+          }
+        }
+        out << std::setw(this->getTitle().size() + 7) << std::setfill('-') << ""
+          << std::setfill(' ') << '\n';
+      }
+    }
+
+    /****************************************************
+    * a class function that receives as parameter the 
+    * representation of the TV Show as a string and
+    * builds a dynamically allocated object of type TvShow
+    * using the information from the string and returns it 
+    * to the client. The parameter contains a single line of
+    * text extracted from the file tvShows.csv
+    * The format is the following:
+    *    ID,TITLE,YEAR,SUMMARY
+    * The function should remove all spaces from the beginning
+    * and end of any token in the string
+    *****************************************************/
+    TvShow* TvShow::createItem(const std::string& strShow)
+    {
+        //YNWA
+    }
+
+    /*************************************************
+    * a class function that function builds an episode
+    *  with the information from the string, searches in
+    *  the collection for a TV show with the specified
+    *  id, and adds it to the list of episodes of the 
+    * found show. The string parameter contains a single
+    *  line of text extracted from the file episodes.csv 
+    * Output should have this format:
+    * ID,EPISODE_NUMBER,SEASON_NUMBER,EPISODE_IN_SEASON,AIR_DATE,LENGTH,TITLE,SUMMARY
+    ***************************************************/
+    template<typename Collection_t>
+    void addEpisode(Collection_t& col, const std::string& strEpisode)
+    {
+
+    }
+
+    //get avgerage length in seconds of an episode
+    // Must use STL algorithms
+    //no manual loops
+    //lambda should not capture anything from the contest by reference
+    double TvShow::getEpsiodeAverageLength() const
+    {
+
+    }
+
+    /*******************************************************
+    * get list with episode names that are at least 1 our long
+    ********************************************************/
+    std::list<std::string> TvShow::getLongEpisodes() const
+
+
+
+}
--- ngena_test15/tvShow.h
+++ ngena_test15/tvShow.h
@@ -8,12 +8,65 @@
 
 namespace seneca 
 {
+
+
     class TvShow
     {
-        /*************************************************
-        *  It is a child who has never travelled
-        * who says only his mother prepares tasty meals :)
-        **************************************************/
+        /*****************************************************
+        *  TvShow class stores the information for a TV show
+        * m_id, title (inherited), release year (inherited)
+        * the summary (inherited), m_epsiode
+        ******************************************************/
+
+        //inside or outside class?
+        struct TvEpisode
+        {
+            const TvShow* m_show{};
+            unsigned short m_numberOverrall{};
+            unsigned short m_season{};
+            unsigned short m_numberInSeason{};
+            std::string m_airDate{};
+            unsigned int m_length{};
+            std::string m_title{};
+            std::string m_summary{};
+        };
+
+        //add necessary constructors
+        //constructors are private
+    public:
+        /********************************************************
+        * override this function to print info about single book
+        *********************************************************/
+        void display(std::ostream& out) const override
+
+        /*****************************************************************
+        * class function that receives as a parameter the representation
+        * of the TV Show as a string a builds a dynamically allocated object
+        * of type of TVShow using information from the string and returns it to
+        * the client. The parameter contains a single line of text extracted
+        * from the file tvShows.csv.
+        ******************************************************************/
+        TvShow* createItem(const std::string& strShow);
+
+        /*************************************************************
+        * a class function that function builds an episode with the
+        * information from the string, searches in the collection for 
+        * a TV show with the specified id, and adds it to the list of
+        * episodes of the found show.
+        ***************************************************************/
+        template<typename Collection_t>
+        void addEpisode(Collection_t& col, const std::string& strEpisode)
+
+        // get the avg length in seconds of the episode
+        //MUST accomplish this using STL Algorithms and NO manual loops
+        // The lambda expression should not capture anything from the context by reference
+        double getEpisodeAverageLength() const:
+
+        //create a list with episode names that are at least 1 hour long
+        //MUST accomplish this using STL Algorithms and NO manual loops
+        // The lambda expression should not capture anything from the context by reference
+        std::list<std::string> getLongEpisodes() const 
+
     };
 }
 #endif
