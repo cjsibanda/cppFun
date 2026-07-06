7f6db8d52b68f617ec6f429d4b5f8368bb22fc67
cjsibanda <anesib100@gmail.com>
2026-07-06 06:00:32 -0400

    started spellChecker.h

---
+++ ngena_test15/spellChecker.h
@@ -0,0 +1,15 @@
+#ifndef SENECA_SPELLCHECKER_H
+#define SENECA_SPELLCHECKER_H
+
+#include <vector>
+#include <string>
+#include <list>
+#include <ostream>
+
+namespace seneca {
+  class SpellChecker
+  {
+    //YNWA
+  }
+}
+#endif
