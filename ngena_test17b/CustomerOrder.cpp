3a58bc60ea875e86b03450d249a448ac53cf1825
cjsibanda <anesib100@gmail.com>
2026-07-21 16:03:53 -0400

    fixed typo

--- ngena_test17b/CustomerOrder.cpp
+++ ngena_test17b/CustomerOrder.cpp
@@ -171,7 +171,7 @@ namespace seneca
         os << m_name << " - " << m_product << "\n";
         for (size_t i = 0; i < m_cntItem; ++i) {
             os << "[" << std::setw(6) << std::setfill('0') << std::right << m_listItem[i]->m_serialNumber << "] ";
-            os << std::setw(m_widthField) << std::setfill(' ') << std::left << m_lstItem[i]->m_itemName << " - ";
+            os << std::setw(m_widthField) << std::setfill(' ') << std::left << m_listItem[i]->m_itemName << " - ";
             os << (m_lstItem[i]->m_isFilled ? "FILLED" : "TO BE FILLED") << "\n";
         }
     }
